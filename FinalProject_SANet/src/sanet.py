2ef5b0afb5ed82c03955d2ec7a06b5dc6bd08442
lev1khachatryan <levon.khachatryan.1996.db@gmail.com>
2019-12-26 15:01:07 +0400

    improvement / Self-Attentional Mechanism for style attentional network

--- FinalProject_SANet/src/sanet.py
+++ FinalProject_SANet/src/sanet.py
@@ -11,11 +11,14 @@ class SANet:
     by slightly modifying the self-attention mechanism
     '''
 
-    def __init__(self, content, style, channels, scope='attention'):
+    def __init__(self, num_filter):
+        self.num_filter = num_filter
+
+    def map(self, content, style, scope='attention'):
         with tf.variable_scope(scope):
-            f = conv(content, channels // 8, kernel=1, stride=1, scope='f_conv') # [bs, h, w, c']
-            g = conv(style,   channels // 8, kernel=1, stride=1, scope='g_conv') # [bs, h, w, c']
-            h = conv(style,   channels     , kernel=1, stride=1, scope='h_conv') # [bs, h, w, c]
+            f = conv(content, self.num_filter // 8, kernel=1, stride=1, scope='f_conv') # [bs, h, w, c']
+            g = conv(style,   self.num_filter // 8, kernel=1, stride=1, scope='g_conv') # [bs, h, w, c']
+            h = conv(style,   self.num_filter     , kernel=1, stride=1, scope='h_conv') # [bs, h, w, c]
 
             # N = h * w
             s = tf.matmul(hw_flatten(g), hw_flatten(f), transpose_b=True) # # [bs, N, N]
@@ -26,13 +29,13 @@ class SANet:
             gamma = tf.get_variable("gamma", [1], initializer=tf.constant_initializer(0.0))
 
             o = tf.reshape(o, shape=content.shape) # [bs, h, w, C]
-            o = conv(o, channels, kernel=1, stride=1, scope='attn_conv')
+            o = conv(o, self.num_filter, kernel=1, stride=1, scope='attn_conv')
 
             o = gamma * o + content
 
             return o
 
-def conv(x, channels, kernel=4, stride=2, pad=0, pad_type='zero', use_bias=True, scope='conv_0'):
+def conv(x, num_filter, kernel=4, stride=2, pad=0, pad_type='zero', use_bias=True, scope='conv_0'):
     with tf.variable_scope(scope):
         if pad > 0:
             h = x.get_shape().as_list()[1]
@@ -51,7 +54,7 @@ def conv(x, channels, kernel=4, stride=2, pad=0, pad_type='zero', use_bias=True,
             if pad_type == 'reflect':
                 x = tf.pad(x, [[0, 0], [pad_top, pad_bottom], [pad_left, pad_right], [0, 0]], mode='REFLECT')
 
-        x = tf.layers.conv2d(inputs=x, filters=channels,
+        x = tf.layers.conv2d(inputs=x, filters=num_filter,
                              kernel_size=kernel, kernel_initializer=weight_init,
                              kernel_regularizer=weight_regularizer,
                              strides=stride, use_bias=use_bias)
