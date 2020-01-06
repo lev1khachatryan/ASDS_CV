3c2a8b0da4b6f15d55993d398baed1eae4d96f1e
lev1khachatryan <levon.khachatryan.1996.db@gmail.com>
2020-01-07 00:45:36 +0400

    bugfix / Comment conv2d - for escape mixture in conv function

--- FinalProject_SANet/src/libs/functions.py
+++ FinalProject_SANet/src/libs/functions.py
@@ -4,38 +4,42 @@ Useful functions, specific for all module parts of code
 
 import numpy as np
 import tensorflow as tf
-# from utils import *
+import tensorflow.contrib as tf_contrib
 
-def conv2d(x, kernel, bias, use_relu=True):
-    '''
-    Convolution operation with reflect padding and valid.
-    Parameters
-    ----------
-    x : array_like
-        Four dimension tensor (batch_size, height, width, channels)
-    kernel : ndarray
-        Kernel we want to apply
-    bias : ndarray
-        bias we want to add
-    use_relu : bool, optional
-        Choose whether use relu or not 
-    Returns:
-    ----------
-    out : array_like
-        convoluted x with kernel and added by bias
-    '''
+weight_init = tf_contrib.layers.xavier_initializer()
+weight_regularizer = None
 
-    # padding image with reflection mode
-    x_padded = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='REFLECT')
+# def conv2d(x, kernel, bias, use_relu=True):
+#     '''
+#     Convolution operation with reflect padding and valid.
+#     Parameters
+#     ----------
+#     x : array_like
+#         Four dimension tensor (batch_size, height, width, channels)
+#     kernel : ndarray
+#         Kernel we want to apply
+#     bias : ndarray
+#         bias we want to add
+#     use_relu : bool, optional
+#         Choose whether use relu or not 
+#     Returns:
+#     ----------
+#     out : array_like
+#         convoluted x with kernel and added by bias
+#     '''
 
-    # conv and add bias
-    out = tf.nn.conv2d(x_padded, kernel, strides=[1, 1, 1, 1], padding='VALID')
-    out = tf.nn.bias_add(out, bias)
+#     # padding image with reflection mode
+#     x_padded = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='REFLECT')
 
-    if use_relu:
-        out = tf.nn.relu(out)
+#     # conv and add bias
+#     out = tf.nn.conv2d(x_padded, kernel, strides=[1, 1, 1, 1], padding='VALID')
+#     # out = tf.nn.conv2d(x_padded, kernel, strides=[1, 1, 1, 1], padding='SAME')
+#     out = tf.nn.bias_add(out, bias)
 
-    return out
+#     if use_relu:
+#         out = tf.nn.relu(out)
+
+#     return out
 
 
 def upsample(x, scale=2):
@@ -94,29 +98,29 @@ def conv(x, num_filter, kernel=4, stride=2, pad=0, pad_type='zero', use_bias=Tru
         convoluted x with kernel and added by bias
     '''
 
-    with tf.variable_scope(scope):
-        if pad > 0:
-            h = x.get_shape().as_list()[1]
-            if h % stride == 0:
-                pad = pad * 2
-            else:
-                pad = max(kernel - (h % stride), 0)
+    # with tf.variable_scope(scope):
+    if pad > 0:
+        h = x.get_shape().as_list()[1]
+        if h % stride == 0:
+            pad = pad * 2
+        else:
+            pad = max(kernel - (h % stride), 0)
 
-            pad_top = pad // 2
-            pad_bottom = pad - pad_top
-            pad_left = pad // 2
-            pad_right = pad - pad_left
+        pad_top = pad // 2
+        pad_bottom = pad - pad_top
+        pad_left = pad // 2
+        pad_right = pad - pad_left
 
-            if pad_type == 'zero':
-                x = tf.pad(x, [[0, 0], [pad_top, pad_bottom], [pad_left, pad_right], [0, 0]])
-            if pad_type == 'reflect':
-                x = tf.pad(x, [[0, 0], [pad_top, pad_bottom], [pad_left, pad_right], [0, 0]], mode='REFLECT')
+        if pad_type == 'zero':
+            x = tf.pad(x, [[0, 0], [pad_top, pad_bottom], [pad_left, pad_right], [0, 0]])
+        if pad_type == 'reflect':
+            x = tf.pad(x, [[0, 0], [pad_top, pad_bottom], [pad_left, pad_right], [0, 0]], mode='REFLECT')
 
-        x = tf.layers.conv2d(inputs=x, filters=num_filter,
-                             kernel_size=kernel, kernel_initializer=weight_init,
-                             kernel_regularizer=weight_regularizer,
-                             strides=stride, use_bias=use_bias)
-        return x
+    x = tf.layers.conv2d(inputs=x, filters=num_filter,
+                         kernel_size=kernel, kernel_initializer=weight_init,
+                         kernel_regularizer=weight_regularizer,
+                         strides=stride, use_bias=use_bias, padding='SAME')
+    return x
 
 def hw_flatten(x) :
     '''
