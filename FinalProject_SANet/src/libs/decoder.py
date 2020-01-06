b9c1f51b46faf53db93f0add330aeba6d724ff4e
lev1khachatryan <levon.khachatryan.1996.db@gmail.com>
2020-01-07 00:43:37 +0400

    story / Decoder mostly mirrors the encoder with all pooling layers replaced

--- FinalProject_SANet/src/libs/decoder.py
+++ FinalProject_SANet/src/libs/decoder.py
@@ -96,3 +96,35 @@ class Decoder:
                 out = upsample(out)
 
         return out
+
+def conv2d(x, kernel, bias, use_relu=True):
+    '''
+    Convolution operation with reflect padding and valid.
+    Parameters
+    ----------
+    x : array_like
+        Four dimension tensor (batch_size, height, width, channels)
+    kernel : ndarray
+        Kernel we want to apply
+    bias : ndarray
+        bias we want to add
+    use_relu : bool, optional
+        Choose whether use relu or not 
+    Returns:
+    ----------
+    out : array_like
+        convoluted x with kernel and added by bias
+    '''
+
+    # padding image with reflection mode
+    x_padded = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='REFLECT')
+
+    # conv and add bias
+    out = tf.nn.conv2d(x_padded, kernel, strides=[1, 1, 1, 1], padding='VALID')
+    # out = tf.nn.conv2d(x_padded, kernel, strides=[1, 1, 1, 1], padding='SAME')
+    out = tf.nn.bias_add(out, bias)
+
+    if use_relu:
+        out = tf.nn.relu(out)
+
+    return out
