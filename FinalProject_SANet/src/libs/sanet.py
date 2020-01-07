d0fd4c88a17bd9671be6fa4415996153df98fe6e
lev1khachatryan <levon.khachatryan.1996.db@gmail.com>
2020-01-07 20:04:08 +0400

    story / Self attention mechanism for style transfer

--- FinalProject_SANet/src/libs/sanet.py
+++ FinalProject_SANet/src/libs/sanet.py
@@ -22,40 +22,19 @@ class SANet:
     def __init__(self, num_filter):
         self.num_filter = num_filter
 
-    # def map(self, content, style, scope='attention'):
-    #     with tf.variable_scope(scope, reuse = tf.AUTO_REUSE):
-    #         f = conv(content, self.num_filter // 8, kernel=1, stride=1, scope='f_conv') # [bs, h, w, c']
-    #         g = conv(style,   self.num_filter // 8, kernel=1, stride=1, scope='g_conv') # [bs, h, w, c']
-    #         h = conv(style,   self.num_filter     , kernel=1, stride=1, scope='h_conv') # [bs, h, w, c]
-
-    #         # N = h * w
-    #         s = tf.matmul(hw_flatten(g), hw_flatten(f), transpose_b=True) # # [bs, N, N]
-
-    #         attention = tf.nn.softmax(s)  # attention map
-
-    #         o = tf.matmul(attention, hw_flatten(h)) # [bs, N, C]
-    #         # gamma = tf.get_variable("gamma", [1], initializer=tf.constant_initializer(0.0))
-
-    #         o = tf.reshape(o, shape=content.shape) # [bs, h, w, C]
-    #         o = conv(o, self.num_filter, kernel=1, stride=1, scope='attn_conv')
-
-    #         # o = gamma * o + content
-    #         o = o + content
-
-    #         return o
     def map(self, content, style, scope='attention'):
         with tf.variable_scope(scope, reuse = tf.AUTO_REUSE):
             f = conv(content, self.num_filter // 8, kernel=1, stride=1, scope='f_conv') # [bs, h, w, c']
             g = conv(style,   self.num_filter // 8, kernel=1, stride=1, scope='g_conv') # [bs, h, w, c']
             h = conv(style,   self.num_filter     , kernel=1, stride=1, scope='h_conv') # [bs, h, w, c]
 
-            s = tf.matmul(g, f, transpose_b=True)
+            s = tf.matmul(hw_flatten(g), hw_flatten(f), transpose_b=True) # [bs, N, N]  N = h * w
 
-            attention = tf.nn.softmax(s)
+            attention = tf.nn.softmax(s)  # attention map
 
-            o = tf.matmul(attention, h)
- 
-            # o = tf.reshape(o, shape=content.shape)
+            o = tf.matmul(attention, hw_flatten(h)) # [bs, N, C]
+
+            o = tf.reshape(o, shape=tf.shape(content)) # [bs, h, w, C]
             o = conv(o, self.num_filter, kernel=1, stride=1, scope='attn_conv')
 
             o = o + content
